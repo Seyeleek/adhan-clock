77e9bcd850b2da7eaceaadf86b41af0b685745e6
ruayfi <327628951+Ro-B-123@users.noreply.github.com>
2026-09-10 13:37:42 -0500

    Revert "Removed the x86_64-linux-android target and used a OnceLock for the weak reference to the app."
    This reverts commit bd630625433e323857ef157b33a3db6637f49b8f.

--- src/app_logic.rs
+++ src/app_logic.rs
@@ -3,7 +3,7 @@ use std::{
     thread,
     time::Duration,
     fs::{OpenOptions, self},
-    sync::{mpsc, Mutex, Arc, OnceLock},
+    sync::{mpsc, Mutex, Arc},
     collections::VecDeque,
     io::{Cursor, Write},
     fmt::Display,
@@ -26,7 +26,6 @@ static LOG_LOCK: Mutex<()> = Mutex::new(());
 static TIMINGS: Mutex<VecDeque<Timing>> = Mutex::new(VecDeque::new());
 static COUNTRY: Mutex<String> = Mutex::new(String::new());
 static CITY: Mutex<String> = Mutex::new(String::new());
-static WEAKAPP: OnceLock<slint::Weak<Clock>> = OnceLock::new();
 
 type Timing = (String, DateTime<Local>, usize);
 
@@ -39,7 +38,6 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
     }));
     load_location(file_prefix);
     let app = Clock::new().unwrap();
-    let _ = WEAKAPP.set(app.as_weak());
     app.on_city_picked(move |new_city| {
         thread::spawn(move || {
             let mut city = CITY.lock().unwrap();
@@ -49,7 +47,9 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
             load_data(file_prefix);
         });
     });
+    let weakapp = app.as_weak();
     app.on_country_picked(move |new_country| {
+        let app = weakapp.clone();
         thread::spawn(move || {
             let mut country = COUNTRY.lock().unwrap();
             *country = new_country.clone().into();
@@ -62,14 +62,16 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
             fs::write(format!("{}city.txt", file_prefix), (*city).clone()).unwrap();
             drop(city);
             let cities: Vec<StandardListViewItem> = cities.iter().map(|&&x| x.into()).collect();
-            ensure_run_in_event_loop(file_prefix, move |app| {
+            ensure_run_in_event_loop(file_prefix, app, move |app| {
                 app.set_cities(cities.as_slice().into());
                 app.invoke_update_city(0);
             });
             load_data(file_prefix);
         });
     });
-    init_city_country();
+    let weakapp = app.as_weak();
+    init_city_country(weakapp);
+    let weakapp = app.as_weak();
     let (time_tx, time_rx) = mpsc::channel();
     let (adhan_tx, adhan_rx) = mpsc::channel();
     let (srise_tx, srise_rx) = mpsc::channel();
@@ -88,13 +90,14 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
             time_tx.send(current_time).unwrap();
             let time = current_time.format("%I:%M:%S %p").to_string();
             let date = current_time.format("%a %m-%d-%Y").to_string();
-            WEAKAPP.get().unwrap().upgrade_in_event_loop(move |app| {
+            weakapp.upgrade_in_event_loop(move |app| {
                 app.set_time(time.into());
                 app.set_date(date.into());
             }).unwrap();
             thread::sleep(SLEEP_TIME);
         }
     });
+    let weakapp = app.as_weak();
     thread::spawn(move || {
         load_data(file_prefix);
         let timings = TIMINGS.lock().unwrap();
@@ -109,7 +112,8 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
             time: x.1.format("%I:%M").to_string().into(),
             ampm: x.1.format("%p").to_string().into(),
         }).collect();
-        ensure_run_in_event_loop(file_prefix, move |app| {
+        let app = weakapp.clone();
+        ensure_run_in_event_loop(file_prefix, app, move |app| {
             app.invoke_update_prayer_times(prayer_times.as_slice().into());
             app.invoke_update_current_prayer(current_prayer.into());
         });
@@ -131,7 +135,8 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
                     time: x.1.format("%I:%M").to_string().into(),
                     ampm: x.1.format("%p").to_string().into(),
                 }).collect();
-                ensure_run_in_event_loop(file_prefix, move |app| {
+                let app = weakapp.clone();
+                ensure_run_in_event_loop(file_prefix, app, move |app| {
                     app.invoke_update_prayer_times(prayer_times.as_slice().into());
                     app.invoke_update_current_prayer(current_prayer.into());
                 });
@@ -146,7 +151,8 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
                 time_left.num_seconds() % 60,
             );
             let next_prayer = format!("{} in\n{}", next_prayer.0, time_left);
-            ensure_run_in_event_loop(file_prefix, move |app| {
+            let app = weakapp.clone();
+            ensure_run_in_event_loop(file_prefix, app, move |app| {
                 app.set_next_prayer(next_prayer.into());
             });
             if timings[0] != current_times[timings[0].2] {
@@ -159,7 +165,8 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
                     time: x.1.format("%I:%M").to_string().into(),
                     ampm: x.1.format("%p").to_string().into(),
                 }).collect();
-                ensure_run_in_event_loop(file_prefix, move |app| {
+                let app = weakapp.clone();
+                ensure_run_in_event_loop(file_prefix, app, move |app| {
                     app.invoke_update_prayer_times(prayer_times.as_slice().into());
                     app.invoke_update_current_prayer(current_prayer.into());
                 });
@@ -177,7 +184,8 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
                     time: x.1.format("%I:%M").to_string().into(),
                     ampm: x.1.format("%p").to_string().into(),
                 }).collect();
-                ensure_run_in_event_loop(file_prefix, move |app| {
+                let app = weakapp.clone();
+                ensure_run_in_event_loop(file_prefix, app, move |app| {
                     app.invoke_update_prayer_times(prayer_times.as_slice().into());
                     app.invoke_update_current_prayer(current_prayer.into());
                     app.set_adhan_playing(true);
@@ -190,6 +198,7 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
             }
         }
     });
+    let weakapp = app.as_weak();
     thread::spawn(move || {
         loop {
             log(file_prefix, "INFO: Adhan loop advanced");
@@ -207,17 +216,20 @@ pub fn main_window(file_prefix: &'static str) -> Clock {
                 sink_handle.mixer(),
                 Cursor::new(sound),
             ).unwrap().sleep_until_end();
-            ensure_run_in_event_loop(file_prefix, move |app| {
+            let app = weakapp.clone();
+            ensure_run_in_event_loop(file_prefix, app, move |app| {
                 app.set_adhan_playing(false);
             });
             log(file_prefix, "INFO: Sound ended");
         }
     });
+    let weakapp = app.as_weak();
     thread::spawn(move || {
         loop {
             srise_rx.recv().unwrap();
             thread::sleep(Duration::from_mins(15));
-            ensure_run_in_event_loop(file_prefix, move |app| {
+            let app = weakapp.clone();
+            ensure_run_in_event_loop(file_prefix, app, move |app| {
                 app.set_adhan_playing(false);
             });
         }
@@ -350,7 +362,7 @@ fn load_location(file_prefix: &'static str) {
     *city = city_data;
 }
 
-fn init_city_country() {
+fn init_city_country(weakapp: slint::Weak<Clock>) {
     let mut countries: Vec<_> = CITIES.keys().collect();
     countries.sort();
     let country = COUNTRY.lock().unwrap();
@@ -361,21 +373,22 @@ fn init_city_country() {
     let city_pos = cities.binary_search(&&(*city).as_str()).unwrap();
     let cities: Vec<StandardListViewItem> = cities.iter().map(|&&x| x.into()).collect();
     let countries: Vec<StandardListViewItem> = countries.iter().map(|&&x| x.into()).collect();
-    let app = WEAKAPP.get().unwrap().unwrap();
+    let app = weakapp.unwrap();
     app.set_city(city_pos as i32);
     app.set_country(country_pos as i32);
     app.set_cities(cities.as_slice().into());
     app.set_countries(countries.as_slice().into());
 }
 
-fn ensure_run_in_event_loop<F>(file_prefix: &'static str, func: F)
-where F: FnOnce(Clock) + Send + Clone + 'static {
-    let result = Arc::new(Mutex::new(Some(())));
+fn ensure_run_in_event_loop<T, U>(file_prefix: &'static str, app: slint::Weak<T>, func: U)
+where T: slint::StrongHandle + 'static, U: FnOnce(T) + Send + Clone + 'static {
     loop {
+        let result = Arc::new(Mutex::new(Some(())));
         let func = func.clone();
+        let app = app.clone();
         let res_el = Arc::clone(&result);
         slint::invoke_from_event_loop(move || {
-            let app = match WEAKAPP.get().unwrap().upgrade() {
+            let app = match app.upgrade() {
                 Some(x) => x,
                 None => {
                     *res_el.lock().unwrap() = None;
