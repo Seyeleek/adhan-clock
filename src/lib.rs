57da095e63c952aeb45b30bafe9790b06748026c
ruayfi <327628951+Seyeleek@users.noreply.github.com>
2026-09-10 19:45:14 -0500

    Gated lib.rs behind #[cfg(target_os = "android")].

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+#[cfg(target_os = "android")]
 mod app_logic;
+#[cfg(target_os = "android")]
 mod cities;
 
+#[cfg(target_os = "android")]
 use slint::{ComponentHandle,
     android::{
         AndroidApp,
@@ -10,7 +13,9 @@ use slint::{ComponentHandle,
     },
 };
 
+#[cfg(target_os = "android")]
 const FILE_PREFIX: &str = "/storage/emulated/0/Android/data/io.github.Seyeleek.adhan_clock/files/";
+#[cfg(target_os = "android")]
 const SAFELY_PRUNE: bool = true;
 
 #[cfg(target_os = "android")]
